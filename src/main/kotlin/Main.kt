fbf91f01b40db40fa88ff6580bd923c986c4c331
AnandaAp <ananda.apriliansyah@ti.ukdw.ac.id>
2021-09-03 14:27:56 +0700

    penambahan fitur

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,15 @@
 fun main() {
+    println("=============================================================================")
+    println("Mesin Pemecah Nama")
+    println("=============================================================================")
     println("Masukan Nama anda untuk dipecah namanya: ")
+
+    //variables
     val nama = readLine().toString()
     val namaHabisDipecah = arrayListOf<String>()
     var temp = String()
+
+    //loop
     for(i in nama.indices){
         //logic untuk menampilkan nama sampai ke sebelum whitespace (spasi)
         if(nama[i] == ' '){
@@ -15,13 +22,14 @@ fun main() {
         dikarenakan ketika i sudah mencapai limit, dan terjadi error karena masih melakukan pengecekan
         maka saya tambahkan pengecekan apakah sudah mencapai limit, jika sudah maka perulangan berhenti
          */
-        else if(i == nama.length){
-            break
-        }
         //Jika belum sampai whitespace, maka akan menampilkan perhuruf
         else{
             temp += nama[i].toString()
             print(nama[i])
+            if (i == nama.length - 1){
+                namaHabisDipecah.add(temp)
+                break
+            }
         }
     }
     print("\nTampilkan:\n1. Nama Depan\n2. Nama Belakang\nTuliskan Pilihanmu: ")
